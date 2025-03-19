abd53f90d369283249a7fa730a0861604db79580
Majdham000 <majdhamm00@gmail.com>
2025-03-19 13:32:43 +0300

    Response format fixed

--- src/controllers/prompt.controller.ts
+++ src/controllers/prompt.controller.ts
@@ -1,13 +1,14 @@
 import { GrpcMethod } from '@nestjs/microservices';
 import { OpenAiService } from '../services/openai.service';
 import { Controller} from '@nestjs/common';
+import { json } from 'node:stream/consumers';
 
 @Controller()
 export class PromptService {
     constructor(private readonly openAIService: OpenAiService) {}
 
     @GrpcMethod('PromptService', 'GeneratePrompt')
-    async generatePrompt(data: { company_data: string; messages_history: string; ideal_answer: string; ai_agent_answer: string }): Promise<{ prompt: string }> {
+    async generatePrompt(data: { company_data: string; messages_history: string; ideal_answer: string; ai_agent_answer: string }): Promise<{ score: number; reason: string }> {
         // const firstKey = Object.keys(data)[0];
         // const firstValue = data[firstKey];
 
@@ -15,7 +16,11 @@ export class PromptService {
         // console.log('Received gRPC request:', firstKey);
         // console.log(typeof(firstValue));
 
-        const prompt = await this.openAIService.generateText(data[Object.keys(data)[0]], data[Object.keys(data)[1]], data[Object.keys(data)[2]], data[Object.keys(data)[3]]);
-        return { prompt };
+        const result = await this.openAIService.generateText(data[Object.keys(data)[0]], data[Object.keys(data)[1]], data[Object.keys(data)[2]], data[Object.keys(data)[3]]);
+        const result_json = JSON.parse(result)
+        //console.log(result_json);
+        
+
+        return { score: result_json.score, reason: result_json.reason };
     }
 }
